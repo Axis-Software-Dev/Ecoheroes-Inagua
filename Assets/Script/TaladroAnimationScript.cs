1106859f34ca52d9e2f9231fc2dac7cb9aaea8ea
OsmarFrausto <osmarherfr@gmail.com>
2025-11-12 18:14:33 -0600

    Gestionar Taladro Animation
    Se configuro la animacion del taladro para reproducirse despues de presionar el boton y 2 segundos despues y se sincronizo con las particulas

---
+++ Assets/Script/TaladroAnimationScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using UnityEngine;
+
+
+//If someone reads this. i want to say i needed to do this script due to the awful way this animation is orginnized
+public class TaladroAnimationScript : MonoBehaviour
+{
+    public GameObject dirtParticle;
+    public GameObject rockParticle;
+    public Animator drillAnimator;
+    public Animator cableAnimator;
+    public float[] intervals;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if(drillAnimator!=null)drillAnimator.enabled = false;
+        if (cableAnimator != null) cableAnimator.enabled = false;
+        dirtParticle?.SetActive(false);
+        rockParticle?.SetActive(false); 
+    }
+
+    public void PlayDrillAnimation()
+    {
+        StartCoroutine(SetDrillAnimation(intervals));
+    }
+
+    private IEnumerator SetDrillAnimation(float[] DelayIntervals)
+    {
+        yield return new WaitForSeconds(intervals[0]);
+        if (drillAnimator != null) drillAnimator.enabled = true;
+        if (cableAnimator != null) cableAnimator.enabled = true;
+        yield return new WaitForSeconds(intervals[1]);
+        dirtParticle?.SetActive(true);
+        rockParticle?.SetActive(true);
+    }
+}
